77b02ed7cc02b5e0a99fc02974e75c281979ff8f
INENCO <rdledesma>
2025-11-18 12:45:26 -0300

    add changes

--- api.py
+++ api.py
@@ -72,4 +72,4 @@ def plot():
 if __name__ == "__main__":
     # Ejecuta primero el job para tener algo al iniciar
     job()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
